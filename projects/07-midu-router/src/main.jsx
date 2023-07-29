adc8c548c0c9ff3b99c98b9cb3ba0e02d2e9f1de
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-07-29 13:58:13 -0600

    feat(projects/07-midu-router): Initial project and package router demo

--- projects/07-midu-router/src/main.jsx
+++ projects/07-midu-router/src/main.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createRoot } from 'react-dom/client'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+import App from './App.jsx'
+
+const root = createRoot(document.getElementById('root'))
+
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
