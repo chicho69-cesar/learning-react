eb50ab2613d80364da7f2c59a8007755001016b8
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-07-30 20:19:31 -0600

    feat(projects/08-todo-app-typescript): Init project structure TODO app with typescript

---
+++ projects/08-todo-app-typescript/.prettierrc.cjs
@@ -0,0 +1,12 @@
+/** @type {import("prettier").Options} */
+const config = {
+  bracketSpacing: true,
+  singleQuote: true,
+  endOfLine: 'auto',
+  tabWidth: 2,
+  useTabs: false,
+  trailingComma: 'none',
+  semi: false
+}
+
+module.exports = config
