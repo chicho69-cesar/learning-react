775b5c5d3fac2c3611c4e4bc482497e7d3194bdd
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-08-02 21:19:30 -0600

    feat(projects/09-google-translate-clone): Add google translate functional project

--- projects/09-google-translate-clone/src/App.tsx
+++ projects/09-google-translate-clone/src/App.tsx
@@ -1,34 +1,129 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import 'bootstrap/dist/css/bootstrap.min.css'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useEffect } from 'react'
+import { Container, Row, Col, Button, Stack } from 'react-bootstrap'
+
+import { ArrowsIcon, ClipboardIcon, SpeakerIcon } from './components/Icons'
+import { LanguageSelector } from './components/LanguageSelector'
+import { TextArea } from './components/TextArea'
+import { AUTO_LANGUAGE, VOICE_FOR_LANGUAGE } from './constants'
+import { useStore } from './hooks/useStore'
+import { useDebounce } from './hooks/useDebounce'
+import { translate } from './services/translate'
+import { SectionType } from './types.d'
+
+function App () {
+  const {
+    fromLanguage,
+    fromText,
+    loading,
+    toLanguage,
+    result,
+    interchangeLanguages,
+    setFromLanguage,
+    setFromText,
+    setResult,
+    setToLanguage,
+  } = useStore()
+
+  const debouncedFromText = useDebounce(fromText, 300)
+
+  useEffect(() => {
+    if (debouncedFromText === '') return
+
+    translate({ fromLanguage, text: debouncedFromText, toLanguage })
+      .then((result) => {
+        if (result == null) return
+        setResult(result)
+      })
+      .catch(() => { setResult('Error') })
+  }, [debouncedFromText, fromLanguage, toLanguage])
+
+  const handleClipboard = () => {
+    window.navigator.clipboard.writeText(result)
+      .catch(() => {})
+  }
+
+  const handleSpeak = () => {
+    const utterance = new SpeechSynthesisUtterance(result)
+    utterance.lang = VOICE_FOR_LANGUAGE[toLanguage]
+    utterance.rate = 0.9
+    speechSynthesis.speak(utterance)
+  }
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Container fluid>
+      <h2>Google Translate</h2>
+
+      <Row>
+        <Col>
+          <Stack gap={2}>
+            <LanguageSelector
+              type={SectionType.From}
+              value={fromLanguage}
+              onChange={setFromLanguage}
+            />
+
+            <TextArea
+              type={SectionType.From}
+              value={fromText}
+              onChange={setFromText}
+            />
+          </Stack>
+        </Col>
+
+        <Col xs='auto'>
+          <Button
+            variant='link'
+            disabled={fromLanguage === AUTO_LANGUAGE}
+            onClick={interchangeLanguages}
+          >
+            <ArrowsIcon />
+          </Button>
+        </Col>
+
+        <Col>
+          <Stack gap={2}>
+            <LanguageSelector
+              type={SectionType.To}
+              value={toLanguage}
+              onChange={setToLanguage}
+            />
+
+            <div style={{ position: 'relative' }}>
+              <TextArea
+                type={SectionType.To}
+                value={result}
+                onChange={setResult}
+                loading={loading}
+              />
+
+              <div style={{
+                position: 'absolute',
+                left: 0,
+                bottom: 0,
+                display: 'flex',
+              }}>
+                <Button
+                  variant='link'
+                  onClick={handleClipboard}
+                >
+                  <ClipboardIcon />
+                </Button>
+
+                <Button
+                  variant='link'
+                  onClick={handleSpeak}
+                >
+                  <SpeakerIcon />
+                </Button>
+              </div>
+            </div>
+          </Stack>
+        </Col>
+      </Row>
+    </Container>
   )
 }
 
--- projects/09-google-translate-clone/src/main.tsx
+++ projects/09-google-translate-clone/src/main.tsx
@@ -1,10 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+
+import App from './App.tsx'
+
+const root = ReactDOM.createRoot(document.getElementById('root')! as HTMLElement)
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
