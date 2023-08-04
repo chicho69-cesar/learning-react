3e551bb15e096ddeef966a009063f72df8582f5b
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-08-04 12:35:32 -0600

    feat(projects/11-typescript-prueba-tecnica): Add technical test for data users displaying and filter

--- projects/11-typescript-prueba-tecnica/src/App.tsx
+++ projects/11-typescript-prueba-tecnica/src/App.tsx
@@ -1,37 +1,136 @@
-import { useState } from 'react';
-import './App.css';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import './App.css'
+
+import { useEffect, useMemo, useRef, useState } from 'react'
+import { SortBy, type User } from './types.d'
+import { UsersList } from './components/UsersList'
 
 function App() {
-	const [count, setCount] = useState(0);
+	const [users, setUsers] = useState<User[]>([])
+	const [showColors, setShowColors] = useState<boolean>(false)
+	const [sorting, setSorting] = useState<SortBy>(SortBy.NONE)
+	const [filterCountry, setFilterCountry] = useState<string | null>(null)
 
-  console.log({ count })
+	// useRef -> para guardar un valor
+  // que queremos que se comparta entre renderizados
+  // pero que al cambiar, no vuelva a renderizar el componente
+	const originalUsers = useRef<User[]>([])
+
+	const toggleColors = () => {
+		setShowColors(!showColors)
+	}
+
+	const toggleSortByCountry = () => {
+		const newSortingValue = sorting === SortBy.NONE ? SortBy.COUNTRY : SortBy.NONE
+		setSorting(newSortingValue)
+	}
+
+	const handleReset = () => {
+		setUsers(originalUsers.current)
+	}
+
+	const handleDelete = (email: string) => {
+		const filteredUsers = users.filter((user) => user.email !== email)
+		setUsers(filteredUsers)
+	}
+
+	const handleChangeSort = (sort: SortBy) => {
+		setSorting(sort)
+	}
+
+	useEffect(() => {
+		fetch('https://randomuser.me/api?results=100')
+			.then((res) => res.json())
+			.then((data) => {
+				setUsers(data.results as User[])
+				originalUsers.current = data.results as User[]
+			})
+			.catch((err) => console.error(err))
+	}, [])
+
+	const filteredUsers = useMemo(() => {
+    console.log('calculate filteredUsers')
+
+		return filterCountry != null && filterCountry.length > 0
+			? users.filter((user) => {
+				return user.location.country
+					.toLowerCase()
+					.includes(filterCountry.toLowerCase())
+			})
+			: users
+	}, [users, filterCountry])
+
+	const sortedUsers = useMemo(() => {
+		console.log('calculate sortedUsers')
+
+		if (sorting === SortBy.NONE) return filteredUsers
+
+		const compareProperties: Record<string, (user: User) => string> = {
+			[SortBy.COUNTRY]: (user) => user.location.country,
+			[SortBy.NAME]: (user) => user.name.first,
+			[SortBy.LAST]: (user) => user.name.last,
+		}
+
+		return filteredUsers.toSorted((a, b) => {
+			const extractProperty = compareProperties[sorting]
+			return extractProperty(a).localeCompare(extractProperty(b))
+		})
+	}, [filteredUsers, sorting])
+
+	/* const filteredUsers = (() => {
+    console.log('calculate filteredUsers')
+    return filterCountry != null && filterCountry.length > 0
+      ? users.filter(user => {
+        return user.location.country.toLowerCase().includes(filterCountry.toLowerCase())
+      })
+      : users
+  })() */
+
+  /* const sortedUsers = (() => {
+    console.log('calculate sortedUsers')
+
+    return sortByCountry
+      ? filteredUsers.toSorted(
+        (a, b) => a.location.country.localeCompare(b.location.country)
+      )
+      : filteredUsers
+  })() */
 
 	return (
-		<>
-			<div>
-				<a href='https://vitejs.dev' target='_blank' rel='noreferrer'>
-					<img src={viteLogo} className='logo' alt='Vite logo' />
-				</a>
-				<a href='https://react.dev' target='_blank' rel='noreferrer'>
-					<img src={reactLogo} className='logo react' alt='React logo' />
-				</a>
-			</div>
-			<h1>Vite + React</h1>
-			<div className='card'>
-				<button onClick={() => setCount((count) => count + 1)}>
-					count is {count}
+		<div className='App'>
+			<h1>Prueba técnica</h1>
+
+			<header>
+				<button onClick={toggleColors}>
+					Colorear filas
 				</button>
-				<p>
-					Edit <code>src/App.tsx</code> and save to test HMR
-				</p>
-			</div>
-			<p className='read-the-docs'>
-				Click on the Vite and React logos to learn more
-			</p>
-		</>
-	);
+
+				<button onClick={toggleSortByCountry}>
+					{sorting === SortBy.COUNTRY ? 'No ordenar por país' : 'Ordenar por país'}
+				</button>
+
+				<button onClick={handleReset}>
+					Restablecer estado
+				</button>
+
+				<input
+					type='text'
+					placeholder='Filtrar por país'
+					onChange={(e) => {
+						setFilterCountry(e.target.value)
+					}}
+				/>
+			</header>
+
+			<main>
+				<UsersList
+					changeSorting={handleChangeSort}
+					deleteUser={handleDelete}
+					showColors={showColors}
+					users={sortedUsers}
+				/>
+			</main>
+		</div>
+	)
 }
 
-export default App;
+export default App
--- projects/11-typescript-prueba-tecnica/src/main.tsx
+++ projects/11-typescript-prueba-tecnica/src/main.tsx
@@ -1,10 +1,7 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import { createRoot } from 'react-dom/client'
+import App from './App'
+
+const root = createRoot(document.querySelector('#root')! as HTMLElement)
+root.render(<App />)
