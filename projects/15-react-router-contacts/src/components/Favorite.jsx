3ca648ebde83d4f3aff2d6e6625f6ad05021aee5
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-08-05 22:26:15 -0600

    feat(projects/15-react-router-contacts): Add project with react router dom v6

---
+++ projects/15-react-router-contacts/src/components/Favorite.jsx
@@ -0,0 +1,26 @@
+import { useFetcher } from 'react-router-dom'
+
+export default function Favorite({ contact }) {
+  const fetcher = useFetcher()
+
+  let favorite = contact.favorite
+  if (fetcher.formData) {
+    favorite = fetcher.formData.get('favorite') === 'true'
+  }
+
+  return (
+    <fetcher.Form method='post'>
+      <button
+        name='favorite'
+        value={favorite ? 'false' : 'true'}
+        aria-label={
+          favorite
+            ? 'Remove from favorites'
+            : 'Add to favorites'
+        }
+      >
+        {favorite ? '★' : '☆'}
+      </button>
+    </fetcher.Form>
+  )
+}
--- projects/15-react-router-contacts/src/main.jsx
+++ projects/15-react-router-contacts/src/main.jsx
@@ -1,10 +1,93 @@
+import './index.css'
+
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom'
+
+import Root, { loader as rootLoader, action as rootAction } from './routes/Root'
+import ErrorPage from './pages/ErrorPage'
+import Index from './routes/Index'
+import Contact, { loader as contactLoader, action as contactAction } from './routes/Contact'
+import EditContact, { action as editContactAction } from './routes/Edit'
+import { action as destroyAction } from './routes/Destroy'
+
+// eslint-disable-next-line no-unused-vars
+const routerFromObjects = createBrowserRouter([
+  {
+    path: '/',
+    element: <Root />,
+    errorElement: <ErrorPage />,
+    loader: rootLoader,
+    action: rootAction,
+    children: [
+      {
+        errorElement: <ErrorPage />,
+        children: [
+          {
+            index: true,
+            element: <Index />,
+          },
+          {
+            path: '/contacts/:contactId',
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: '/contacts/:contactId/edit',
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editContactAction,
+          },
+          {
+            path: '/contacts/:contactId/destroy',
+            action: destroyAction,
+            errorElement: <div>Oops! There was an error.</div>,
+          },
+        ]
+      },
+    ]
+  },
+])
+
+const routerFromJSX = createBrowserRouter(
+  createRoutesFromElements(
+    <Route 
+      path='/'
+      element={<Root />}
+      errorElement={<ErrorPage />}
+      loader={rootLoader}
+      action={rootAction}
+    >
+      <Route errorElement={<ErrorPage />}>
+        <Route index element={<Index />} />
+
+        <Route
+          path='/contacts/:contactId'
+          element={<Contact />}
+          loader={contactLoader}
+          action={contactAction}
+        />
+
+        <Route
+          path='/contacts/:contactId/edit'
+          element={<EditContact />}
+          loader={contactLoader}
+          action={editContactAction}
+        />
+
+        <Route
+          path='/contacts/:contactId/destroy'
+          element={<div>Oops! There was an error.</div>}
+          action={destroyAction}
+        />
+      </Route>
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={routerFromJSX} />
   </React.StrictMode>,
 )
