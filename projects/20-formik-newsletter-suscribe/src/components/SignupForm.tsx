a636e97670a3b9e2698dcebc7ecd48a34d8f2d56
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-09-09 20:31:56 -0600

    feat(projects/20-formik-newsletter-suscribe): Setup basic form with formik

---
+++ projects/20-formik-newsletter-suscribe/src/components/SignupForm.tsx
@@ -0,0 +1,28 @@
+import { useFormik } from 'formik';
+
+export default function SignupForm() {
+  const formik = useFormik({
+    initialValues: { email: '' },
+    onSubmit: (values) => {
+      alert(JSON.stringify(values, null, 2))
+    }
+  })
+
+  return (
+    <form onSubmit={formik.handleSubmit}>
+      <label htmlFor='email'>Email Address</label>
+
+      <input
+        type='email'
+        id='email'
+        name='email'
+        onChange={formik.handleChange}
+        value={formik.values.email}
+      />
+
+      <button type='submit'>
+        Submit
+      </button>
+    </form>
+  )
+}
