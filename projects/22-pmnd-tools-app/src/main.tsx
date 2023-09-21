a441f0ad9aca94a53368969074b733f4716657df
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-09-21 07:30:25 -0600

    feat(projects/22-pmnd-tools-app): Add panda css configuration

--- projects/22-pmnd-tools-app/src/main.tsx
+++ projects/22-pmnd-tools-app/src/main.tsx
@@ -1,10 +1,13 @@
+import './index.css'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import App from './App'
+
+const root = ReactDOM.createRoot(document.getElementById('root')! as HTMLElement)
+
+root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
+  </React.StrictMode>
 )
