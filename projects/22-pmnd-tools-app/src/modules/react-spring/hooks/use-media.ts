a4b1f5fcda6aa12adc618b2fc4df2cdaec77d83f
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-10-07 13:54:49 -0600

    feat(projects/22-pmnd-tools-app): Add config for react stitches styles

---
+++ projects/22-pmnd-tools-app/src/modules/react-spring/hooks/use-media.ts
@@ -0,0 +1,18 @@
+import { useCallback, useEffect, useState } from 'react'
+
+export default function useMedia(queries: string[], values: number[], defaultValue: number) {
+  const match = useCallback(() => {
+    return values[queries.findIndex(q => matchMedia(q).matches)] || defaultValue
+  }, [defaultValue, queries, values])
+  
+  const [value, set] = useState(match)
+  
+  useEffect(() => {
+    const handler = () => set(match)
+    window.addEventListener('resize', handler)
+    
+    return () => window.removeEventListener('resize', handler)
+  }, [match])
+  
+  return value
+}
