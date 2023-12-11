640cd29c733380b8ed0c8c5f52374685bde7507e
Cesar <cesarvillalobosolmos.01@gmail.com>
2023-12-11 13:53:10 -0600

    feat(projects/14-hacker-news-prueba-tecnica): Configure linter and logo for 14 hacker news

--- projects/14-hacker-news-prueba-tecnica/.eslintrc.cjs
+++ projects/14-hacker-news-prueba-tecnica/.eslintrc.cjs
@@ -1,18 +1,16 @@
 module.exports = {
-  root: true,
   env: { browser: true, es2020: true },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
+    './node_modules/ts-standard/eslintrc.json'
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module', project: './tsconfig.json' },
   plugins: ['react-refresh'],
   rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+    'react-refresh/only-export-components': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off'
+  }
 }
